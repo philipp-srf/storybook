e0801b6e6499457e040b701f37bf6ea933a7404d
libetl <libe4@free.fr>
2019-03-20 15:14:56 +0100

    boostrapper script will display an error message instead of cascading with another exception

--- addons/storysource/src/StoryPanel.js
+++ addons/storysource/src/StoryPanel.js
@@ -238,6 +238,7 @@ import { toId } from "@storybook/router/utils";
 import { forceReRender, addDecorator } from "${(idsToFrameworks || {})[
       toId(kind || 'a', story || 'a')
     ] || '@storybook/react'}";
+import { document } from "global";
 import React from "react";
 import {
   Global,
@@ -257,6 +258,12 @@ addDecorator(storyFn => (
 addons.getChannel().emit(Events.SET_CURRENT_STORY, {
   storyId: toId("${kind}", "${story}")
 });
+const div1 = document.createElement("div");
+div1.id = "error-stack";
+const div2 = document.createElement("div");
+div2.id = "error-message";
+document.body.appendChild(div1);
+document.body.appendChild(div2);
 forceReRender();
 `;
 
